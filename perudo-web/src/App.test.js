e5fd01ddaba8620fe304a9814faacefcb1bf04c2
Alexandre Bignalet <alexandre.bignalet@gmail.com>
2022-08-19 11:13:18 +0200

    update webapp

--- perudo-web/src/App.test.js
+++ perudo-web/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders perudo', () => {
+  render(<App/>);
+  const linkElement = screen.getByText(/perudo/i);
   expect(linkElement).toBeInTheDocument();
 });
