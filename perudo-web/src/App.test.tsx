926ef8edcf7f103efb35356144d2b0ca0cf89152
Alexandre Bignalet <alexandre.bignalet@gmail.com>
2022-08-21 13:04:20 +0200

    remove tests

--- perudo-web/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders perudo', () => {
-  render(<App/>);
-  const linkElement = screen.getByText(/perudo/i);
-  expect(linkElement).toBeInTheDocument();
-});
